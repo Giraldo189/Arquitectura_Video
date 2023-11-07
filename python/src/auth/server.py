7e96eda57ea1ff25db0a9f7181824f5bb84563e5
Giraldo189 <miguelgiraldo200@gmail.com>
2023-11-06 22:38:25 -0500

    Inicio_Server

--- python/src/auth/server.py
+++ python/src/auth/server.py
@@ -17,4 +17,10 @@ def login():
     if not auth:
         return "missing credentials", 401
 
-    #check db for username and password 
+    #check db for username and password
+    cur = mysql.connection.cursor()
+    res = cur.execute(
+              "SELECT email, password FROM user WHERE email=%s", (auth.username,)
+            )
+                
+            if res > 0:
